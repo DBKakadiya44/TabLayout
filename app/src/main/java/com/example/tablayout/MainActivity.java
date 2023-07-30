d086d04f5cbdc877f991eef6b94a94acfa1db3dc
DBkakadiya44 <deepkakadiya2002>
2023-07-30 23:47:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tablayout/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.tablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity
+{
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout=findViewById(R.id.tablayout);
+        viewPager=findViewById(R.id.viewpager);
+
+        ViewpagerAdapter adapter = new ViewpagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
